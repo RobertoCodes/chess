4ed5b24f4da2079ad06d80f5dd407daa1fc24b03
App Academy Student <appacademy@Apps-Mac-mini.local>
2015-10-06 11:06:23 -0400

    Add Pawn class and moves method

--- chess_piece.rb
+++ chess_piece.rb
@@ -51,7 +51,7 @@ class SteppingPiece < Piece
       y +=  move[1]
       possible_moves << [x,y] if x.between?(0,7) && y.between?(0,7)
     end
-    
+
     possible_moves.select { |move| board[move[0]][move[1]].nil? ||
       board[move[0]][move[1]].color != self.color }
   end
@@ -112,5 +112,62 @@ class Knight < SteppingPiece
 end
 
 
-class Pawn < SteppingPiece
+class Pawn < Piece
+
+  attr_accessor :first_move
+
+  def initialize(color, pos, board, name = "P", first_move = true)
+    super(color, pos, board, name)
+    @first_move = first_move
+  end
+
+
+
+  def moves
+    possible_moves = []
+
+      if self.color == :black
+        x = self.pos[0]
+        y = self.pos[1]
+        if first_move && board[x+2][y].nil?
+          possible_moves << [x+2,y]
+        end
+
+        if board[x+1][y].nil?
+          possible_moves << [x+1,y]
+        end
+
+        if !board[x+1][y-1].nil? && board[x+1][y-1].color == :white
+          possible_moves << [x+1,y-1]
+        end
+
+        if !board[x+1][y+1].nil? && board[x+1][y+1].color == :white
+            possible_moves << [x+1,y-1]
+        end
+
+      end
+
+      if self.color == :white
+        x = self.pos[0]
+        y = self.pos[1]
+        if first_move && board[x-2][y].nil?
+          possible_moves << [x-2,y]
+        end
+
+        if board[x-1][y].nil?
+          possible_moves << [x-1,y]
+        end
+
+        if !board[x-1][y-1].nil? && board[x-1][y-1].color == :black
+          possible_moves << [x-1,y-1]
+        end
+
+        if !board[x-1][y+1].nil? && board[x-1][y+1].color == :black
+            possible_moves << [x-1,y-1]
+        end
+
+      end
+      possible_moves
+
+  end
 end
